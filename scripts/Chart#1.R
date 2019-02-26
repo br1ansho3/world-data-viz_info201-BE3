f8e3cd0307f40e3d31fa303d3c99e4e4afde3715
Henry Bates <batesh2@uw.edu>
2019-02-25 19:52:17 -0800

    finished chart#1

--- scripts/Chart#1.R
+++ scripts/Chart#1.R
@@ -1,12 +1,40 @@
 library(ggplot2)
 library(dplyr)
+library(plotly)
+library(styler)
+library(lintr)
 # Read in csv file for Chart 1
-small_countries <- read.csv("data/WDVP Datasets - small countries are beautiful.csv", stringsAsFactors = F)
-small_countries_edited <- small_countries[5:76,]
+dataframe_choice <- read.csv(
+  "data/WDVP Datasets - small countries are beautiful.csv",
+  stringsAsFactors = F, na.strings = "-"
+)
 
-# GDP per km2 vs %foreign born 
+# Getting Rid of Column Names
 gdp_area_vs_foreign_born <- function(dataframe_choice) {
-  ggplot(data = dataframe_choice, mapping = aes(x = GDP.per.km2, 
-                                         y = X..foreign.born.population) +
-           geom_bar(stat = "identity"))
+  small_countries_edited <- dataframe_choice[5:76, ]
+  # Getting Rid of NA values and selecting columns
+  for_function <- small_countries_edited %>%
+    select(indicator, GDP, X..foreign.born.population) %>%
+    filter(
+      !is.na(indicator), !is.na(GDP),
+      !is.na(X..foreign.born.population)
+    ) %>%
+    mutate(
+      GDP = as.numeric(as.character(GDP)),
+      X..foreign.born.population = as.numeric(as.character(
+        X..foreign.born.population
+      ))
+    )
+  # Plotting and Setting Layout
+  plot_ly(
+    data = for_function, x = ~ for_function$GDP,
+    y = ~ for_function$X..foreign.born.population,
+    text = for_function$indicator, hoverinfo = "text", color = ~GDP
+  ) %>%
+    layout(
+      title = "GDP vs % Foreign Born",
+      xaxis = list(title = "GDP (Billions)"),
+      yaxis = list(title = "% Foreign Born"),
+      annotations = list(title = "GDP (Billions of $")
+    )
 }
