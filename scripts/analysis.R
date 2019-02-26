999105bc67c974518d3e0315f4c7e3e8daeb1f18
br1ansho3 <brianhsu860906@gmail.com>
2019-02-26 09:47:14 -0800

    complete analysis.R

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,69 +2,45 @@
 #Are any outlier countries worth our attention?
 #Can we learn anything from the trends and patterns of these dataset?
 #Do these countries derive power from their agility? Or is innovation more risky?
-library(dplyr)
-library(lazyeval)
+library(lintr)
 
-
-small_countries_df <- read.csv('data/WDVP Datasets - small countries are beautiful.csv', stringsAsFactors = F, na.strings = "-")
+small_countries_df <- read.csv(
+  "data/WDVP Datasets - small countries are beautiful.csv",
+  stringsAsFactors = F, na.strings = "-")
 
 #remove first 4 rows
 small_countries_df <- small_countries_df[5:nrow(small_countries_df), ]
-#remove all rows that have a missing value
-
 
 #basic information 
-#basic_info$feature#max#country/value
-find_metadata <- function(df, feature) {
-  specific_table <- df[, c("indicator", feature)]
-  specific_table[, feature] <- as.numeric(gsub(",", "", df[, feature]))
-  specific_table
-  max <- specific_table[(specific_table[, feature]) == 
-                          max(specific_table[, feature], na.rm = TRUE) & 
-                          (!is.na((specific_table[, feature]))),]
-  min <- specific_table[(specific_table[, feature]) == 
-                          min(specific_table[, feature], na.rm = TRUE) & 
-                          (!is.na((specific_table[, feature]))),]
-  avg <- mean(specific_table[, feature], na.rm = TRUE)
-  list(max = max, min = min, avg = avg)
+#format: basic_info$feature$max$value
+summarize_information <- function(df) {
+  find_metadata <- function(feature) {
+    specific_table <- df[, c("indicator", feature)]
+    specific_table[, feature] <- as.numeric(gsub(",", "", df[, feature]))
+    specific_table
+    max <- specific_table[(specific_table[, feature]) ==
+                            max(specific_table[, feature], na.rm = TRUE) &
+                            (!is.na((specific_table[, feature]))), ]
+    min <- specific_table[(specific_table[, feature]) ==
+                            min(specific_table[, feature], na.rm = TRUE) &
+                            (!is.na((specific_table[, feature]))), ]
+    avg <- mean(specific_table[, feature], na.rm = TRUE)
+    list(max = max, min = min, avg = avg)
+  }
+  find_metadata("total.foreign.born.population.")
+
+  # list of all features
+  features <- colnames(small_countries_df)[-1]
+  values <- lapply(features, find_metadata)
+  names(values) <- features
+
+  #other info
+  #countries that have a complete dataset
+  complete_rows <- complete.cases(small_countries_df)
+  complete_country <- small_countries_df[complete_rows, "indicator"]
+  num_complete <- length(complete_country)
+
+  values$other <- list(complete_country = complete_country,
+                       num_complete = num_complete)
+  values
 }
-
-
-
-
-#countries that have a complete dataset
-complete_rows <- complete.cases(small_countries_df)
-complete_small_countries <- small_countries_df[complete_rows, "indicator"]
-
-
-
-#TEST AREA
-find_metadata <- function(df, feature) {
-  specific_table <- df[, c("indicator", feature)]
-  specific_table[, feature] <- as.numeric(gsub(",", "", df[, feature]))
-  specific_table
-  max <- specific_table[(specific_table[, feature]) == 
-                          max(specific_table[, feature], na.rm = TRUE) & 
-                          (!is.na((specific_table[, feature]))),]
-  min <- specific_table[(specific_table[, feature]) == 
-                          min(specific_table[, feature], na.rm = TRUE) & 
-                          (!is.na((specific_table[, feature]))),]
-  avg <- mean(specific_table[, feature], na.rm = TRUE)
-  list(max = max, min = min, avg = avg)
-}
-is.na(specific_table[, feature])
-specific_table <- find_metadata(small_countries_df, "GDP.1")
-test <- find_metadata(small_countries_df, "GDP.1")
-#TESTING AREA
-hi <- "GDP.1"
-specific_table <- small_countries_df %>% 
-  select_("indicator", hi) #%>% 
-  #mutate_(hi = gsub(",", "", hi)) 
-specific_table[, hi] <- as.numeric(specific_table[ , hi])
-max <- specific_table %>% 
-  
-  filter_("GDP" == max(GDP, na.rm = TRUE))
-# comparison 
-na.omit(specific_table[,hi]) ==max(specific_table[, hi], na.rm = TRUE)
-specific_table[na.omit(specific_table[, hi]) == max(specific_table[, hi], na.rm = TRUE),] 
-filter_(specific_table, hi == paste0("max(", hi, ", na.rm = TRUE)")) #doesn't work
