2d2109d76fc21fa60882df79e555f7a0d725c9f3
Henry Bates <batesh2@uw.edu>
2019-02-25 16:47:01 -0800

    read in files

--- Chart#1.R
+++ Chart#1.R
@@ -1,4 +1,12 @@
+library(ggplot2)
+library(dplyr)
 # Read in csv file for Chart 1
-dataframe <- read.csv("WDVP Datasets - small countries are beautiful.csv")
+small_countries <- read.csv("data/WDVP Datasets - small countries are beautiful.csv", stringsAsFactors = F)
+small_countries_edited <- small_countries[5:76,]
 
 # GDP per km2 vs %foreign born 
+gdp_area_vs_foreign_born <- function(dataframe_choice) {
+  ggplot(data = dataframe_choice, mapping = aes(x = GDP.per.km2, 
+                                         y = X..foreign.born.population) +
+           geom_bar(stat = "identity"))
+}
